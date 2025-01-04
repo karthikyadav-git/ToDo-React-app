72287350aa16127a4833957464762a6a0de749e8
Karthik Yadav <mail@karthikyadav.com>
2025-01-04 22:21:35 +0545

    added new component "CategoriesTab"

---
+++ src/components/CategoriesTab.tsx
@@ -0,0 +1,11 @@
+
+const CategoriesTab = () => {
+    return (
+        <>
+        <h2 className="categoryTitle">Categories</h2>
+        <p className="categoriesList"></p>
+        </>
+    );
+};
+
+export default CategoriesTab;
